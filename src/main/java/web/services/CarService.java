330d900ecea2c131c25463c0b69a6c10b733d973
GordinVit <gordin.vit@gmail.com>
2024-09-06 00:11:51 +0300

    pp_2_2_2_v2

--- src/main/java/web/services/CarService.java
+++ src/main/java/web/services/CarService.java
@@ -1,9 +1,10 @@
-package web.Services;
+package web.services;
 
 import java.util.List;
+
 import web.models.Car;
 
 public interface CarService {
-  List<Car> getCars( int count);
+    List<Car> getCars(int count);
 
 }
